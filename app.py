df9dd1f155c4995738a65e8678aafc825e032c8d
m3ufdech0c <aidadaz@gmail.com>
2023-04-12 13:29:52 -0500

    revised with different routes

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ CORS(app)
 @ app.route('/get_data')
 def get_data():
     cur = conn.cursor()
-    cur.execute('SELECT * FROM air_pollution;')
+    cur.execute('SELECT * FROM air_pollution LIMIT 100;')
     data = cur.fetchall()
     cur.close()
     headers = [desc[0] for desc in cur.description]
@@ -24,6 +24,38 @@ def get_data():
     print('Data accessed from database:', data)
     return jsonify(result)
 
+@ app.route('/daily_avg')
+def daily_avg():
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM alldaily_avg LIMIT 100;')
+    data = cur.fetchall()
+    cur.close()
+    headers = [desc[0] for desc in cur.description]
+    result = [dict(zip(headers, row)) for row in data]
+    print('Data accessed from database:', data)
+    return jsonify(result)
+
+@ app.route('/feb_avg')
+def feb_avg():
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM feb_avg;')
+    data = cur.fetchall()
+    cur.close()
+    headers = [desc[0] for desc in cur.description]
+    result = [dict(zip(headers, row)) for row in data]
+    print('Data accessed from database:', data)
+    return jsonify(result)
+
+@ app.route('/yearly_avg')
+def yearly_avg():
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM yearly_avg_pollution;')
+    data = cur.fetchall()
+    cur.close()
+    headers = [desc[0] for desc in cur.description]
+    result = [dict(zip(headers, row)) for row in data]
+    print('Data accessed from database:', data)
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(debug=True)
