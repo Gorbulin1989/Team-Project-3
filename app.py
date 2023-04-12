a5f75a1008e1c3e86a9c827ee1ceb3b13452b1cc
Gorbulin1989 <AGorbulin@slb.com>
2023-04-12 17:51:44 -0500

    updated python

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 from flask import Flask, jsonify
-<<<<<<< HEAD
 from decimal import Decimal
-=======
->>>>>>> 57227346347bb6d5305b6425c58190fbc71f7590
 import psycopg2
 import json
 from flask_cors import CORS
 
 conn = psycopg2.connect (
-<<<<<<< HEAD
+
     dbname = 'AQI',
     user = 'postgres',
     password = 'Gorbulin1989!',
@@ -27,7 +24,6 @@ def get_data():
     headers = [desc[0] for desc in cur.description]
     result = [dict(zip(headers, ((float(i) if isinstance(i, Decimal) else i) for i in row))) for row in data]
     return jsonify(result)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/logic.js
+++ static/js/logic.js
@@ -1,19 +1,136 @@
 // Creating a map object.
-var myMap = L.map("map", {
-    center: [14.716677, -17.467686],
-    zoom: 2.2
-  });
+// var myMap = L.map("map", {
+   //  center: [14.716677, -17.467686],
+   // zoom: 2.2
+ // });
   
   // Adding a tile layer.
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(myMap);
+ // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+   //   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+ // }).addTo(myMap);
   
   var url = "http://127.0.0.1:5000/get_data"
   
   // Promise Pending
   const dataPromise = d3.json(url);
   console.log("Data Promise: ", dataPromise);
+
+// Get the data for the selected city.
+function getData(city) {
+  return dataPromise.then(function(data) {
+    return data.filter(function(d) {
+      return d.city === city;
+    });
+  });
+}
+
+// Update the line plot.
+function updatePlot(city) {
+  getData(city).then(function(data) {
+    // Format the date.
+    var parseDate = d3.timeParse("%a, %d %b %Y %H:%M:%S %Z");
+    data.forEach(function(d) {
+      d.date = parseDate(d.date);
+    });
+
+    // Set the dimensions of the plot.
+    var margin = { top: 20, right: 20, bottom: 30, left: 50 };
+    var width = 600 - margin.left - margin.right;
+    var height = 400 - margin.top - margin.bottom;
+
+    // Create the scales.
+    var xScale = d3.scaleTime().range([0, width]);
+    var yScale = d3.scaleLinear().range([height, 0]);
+
+    // Create the axes.
+    var xAxis = d3.axisBottom(xScale);
+    var yAxis = d3.axisLeft(yScale);
+
+    // Create the line functions.
+    var tempLine = d3.line()
+      .x(function(d) { return xScale(d.date); })
+      .y(function(d) { return yScale(d.temperature_2m_deg_c); });
+    var humLine = d3.line()
+      .x(function(d) { return xScale(d.date); })
+      .y(function(d) { return yScale(d.relativehumidity_2m_perc); });
+    var windLine = d3.line()
+      .x(function(d) { return xScale(d.date); })
+      .y(function(d) { return yScale(d.windspeed_10m_kmh); });
+
+    // Create the SVG element.
+    var svg = d3.select("#plot")
+      .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    // Set the domains of the scales.
+    xScale.domain(d3.extent(data, function(d) { return d.date; }));
+    yScale.domain([0, d3.max(data, function(d) {
+      return Math.max(d.temperature_2m_deg_c, d.relativehumidity_2m_perc, d.windspeed_10m_kmh);
+    })]);
+
+    // Add the temperature line.
+    svg.append("path")
+      .datum(data)
+      .attr("class", "line temp-line")
+      .attr("d", tempLine);
+
+ // Add the humidity line.
+ svg.append("path")
+ .datum(data)
+ .attr("class", "line hum-line")
+ .attr("d", humLine);
+
+// Add the wind speed line.
+svg.append("path")
+ .datum(data)
+ .attr("class", "line wind-line")
+ .attr("d", windLine);
+
+// Add the x-axis.
+svg.append("g")
+ .attr("class", "axis x-axis")
+ .attr("transform", "translate(0," + height + ")")
+ .call(xAxis);
+
+// Add the y-axis.
+svg.append("g")
+ .attr("class", "axis y-axis")
+ .call(yAxis);
+
+// Add the axis labels.
+svg.append("text")
+ .attr("class", "axis-label")
+ .attr("transform", "rotate(-90)")
+ .attr("y", 0 - margin.left)
+ .attr("x", 0 - (height / 2))
+ .attr("dy", "1em")
+ .style("text-anchor", "middle")
+ .text("Temperature, Humidity, Wind Speed");
+
+svg.append("text")
+ .attr("class", "axis-label")
+ .attr("y", height + margin.bottom - 5)
+ .attr("x", width / 2)
+ .style("text-anchor", "middle")
+ .text("Date");
+});
+}
+
+// Set the initial city.
+var city = "Beijing";
+
+// Update the plot when the city is changed.
+d3.select("#city-select").on("change", function() {
+city = d3.select(this).property("value");
+d3.selectAll(".line").remove();
+updatePlot(city);
+});
+
+// Update the plot for the initial city.
+updatePlot(city);
   
   // Performing a GET request to the query URL/
   //d3.json(url).then(function (data){
@@ -80,4 +197,3 @@ var myMap = L.map("map", {
   
   
   
-})
